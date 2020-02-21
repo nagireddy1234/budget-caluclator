9b5ad2c49371de097bf7c722951af466834be6a7
Nagireddy P <nagireddy.panditi@gmail.com>
2020-02-21 22:36:11 +0530

    Updates

---
+++ precache-manifest.ae9ee97b80979eacad7137eebd266a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8014d74105e90f9499c5bbf877ce7b36",
+    "url": "/budget-caluclator/index.html"
+  },
+  {
+    "revision": "2c98a46f74a3fa9cfe14",
+    "url": "/budget-caluclator/static/css/main.db945ab5.chunk.css"
+  },
+  {
+    "revision": "d0e67d326575255e7c31",
+    "url": "/budget-caluclator/static/js/2.afe2dbea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/budget-caluclator/static/js/2.afe2dbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c98a46f74a3fa9cfe14",
+    "url": "/budget-caluclator/static/js/main.55e891cb.chunk.js"
+  },
+  {
+    "revision": "7803e2eaaa49178c5b6f",
+    "url": "/budget-caluclator/static/js/runtime-main.4d45278c.js"
+  }
+]);
